2e82cac2534169140bce8cb0f5abd549b4cbd5b7
blizkyone <paolo@blizky.com>
2023-04-05 11:48:07 -0600

    styling

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,16 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+        port: "",
+        pathname: "/oauth_google/**",
+      },
+    ],
+  },
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.
